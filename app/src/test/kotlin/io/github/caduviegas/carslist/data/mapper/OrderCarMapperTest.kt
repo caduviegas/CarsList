6a6de4ce99b0946cb3890a6ee7729adc132d99e2
Eduardo Viegas <engenedu@gmail.com>
2025-07-29 19:01:52 -0300

    🚧 map to OrderCarDTO

---
+++ app/src/test/kotlin/io/github/caduviegas/carslist/data/mapper/OrderCarMapperTest.kt
@@ -0,0 +1,95 @@
+package io.github.caduviegas.carslist.data.mapper
+
+import com.google.common.truth.Truth.assertThat
+import io.github.caduviegas.carslist.data.model.CarInfoDTO
+import io.github.caduviegas.carslist.data.model.OrderCarDTO
+import io.github.caduviegas.carslist.data.model.UserDTO
+import io.github.caduviegas.carslist.domain.model.Car
+import io.github.caduviegas.carslist.domain.model.FuelType
+import io.github.caduviegas.carslist.domain.model.User
+import org.junit.Test
+import java.time.LocalDate
+import java.time.ZoneId
+
+class OrderCarMapperTest {
+
+    private val mapper = OrderCarMapper()
+
+    @Test
+    fun `toUserDTO maps User to UserDTO correctly`() {
+        val birthday = LocalDate.of(1990, 5, 20)
+        val user = User(
+            cpf = "12345678900",
+            name = "João da Silva",
+            email = "joao@email.com",
+            phone = "11999999999",
+            birthday = birthday
+        )
+
+        val userDTO = mapper.toUserDTO(user)
+        val expectedBirthDate = birthday.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000
+
+        assertThat(userDTO.cpf).isEqualTo(user.cpf)
+        assertThat(userDTO.name).isEqualTo(user.name)
+        assertThat(userDTO.email).isEqualTo(user.email)
+        assertThat(userDTO.phone).isEqualTo(user.phone)
+        assertThat(userDTO.birthDate).isEqualTo(expectedBirthDate)
+    }
+
+    @Test
+    fun `toCarInfoDTO maps Car to CarInfoDTO correctly`() {
+        val car = Car(
+            id = 1,
+            cadastro = LocalDate.of(2020, 1, 1),
+            modeloId = 42,
+            ano = 2020,
+            fuelType = FuelType.FLEX,
+            numPortas = 4,
+            cor = "Prata",
+            nomeModelo = "Tesla Model Y",
+            valor = 100000.0
+        )
+
+        val carInfoDTO = mapper.toCarInfoDTO(car)
+
+        assertThat(carInfoDTO.id).isEqualTo(car.id)
+        assertThat(carInfoDTO.modelId).isEqualTo(car.modeloId)
+    }
+
+    @Test
+    fun `toOrderCarDTO maps all fields correctly`() {
+        val birthday = LocalDate.of(1990, 5, 20)
+        val user = User(
+            cpf = "12345678900",
+            name = "João da Silva",
+            email = "joao@email.com",
+            phone = "11999999999",
+            birthday = birthday
+        )
+        val car = Car(
+            id = 1,
+            cadastro = LocalDate.of(2020, 1, 1),
+            modeloId = 42,
+            ano = 2020,
+            fuelType = FuelType.FLEX,
+            numPortas = 4,
+            cor = "Prata",
+            nomeModelo = "Model Y",
+            valor = 100000.0
+        )
+        val orderId = "ORDER123"
+        val orderDate = LocalDate.of(2024, 6, 1)
+        val status = "PENDING"
+
+        val orderCarDTO = mapper.toOrderCarDTO(orderId, orderDate, status, user, car)
+        val expectedOrderDate = orderDate.atStartOfDay(ZoneId.systemDefault()).toEpochSecond() * 1000
+        val expectedUserDTO = mapper.toUserDTO(user)
+        val expectedCarInfoDTO = mapper.toCarInfoDTO(car)
+
+        assertThat(orderCarDTO.orderId).isEqualTo(orderId)
+        assertThat(orderCarDTO.orderDate).isEqualTo(expectedOrderDate)
+        assertThat(orderCarDTO.status).isEqualTo(status)
+        assertThat(orderCarDTO.user).isEqualTo(expectedUserDTO)
+        assertThat(orderCarDTO.car).isEqualTo(expectedCarInfoDTO)
+    }
+}
