76d062636b553fb4ff99a6766da0debc9da2b49c
Eduardo Viegas <engenedu@gmail.com>
2025-07-29 13:12:21 -0300

    🗃️ sqlite User and PedidoCompra entities

---
+++ app/src/main/kotlin/io/github/caduviegas/carslist/db/converter/LocalDateConverter.kt
@@ -0,0 +1,16 @@
+package io.github.caduviegas.carslist.db.converter
+
+import androidx.room.TypeConverter
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneOffset
+
+class LocalDateConverter {
+    @TypeConverter
+    fun fromLocalDate(date: LocalDate?): Long? =
+        date?.atStartOfDay(ZoneOffset.UTC)?.toInstant()?.toEpochMilli()
+
+    @TypeConverter
+    fun toLocalDate(millis: Long?): LocalDate? =
+        millis?.let { Instant.ofEpochMilli(it).atZone(ZoneOffset.UTC).toLocalDate() }
+}
---
+++ app/src/main/kotlin/io/github/caduviegas/carslist/db/entity/PedidoCompra.kt
@@ -0,0 +1,23 @@
+package io.github.caduviegas.carslist.db.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity
+data class PedidoCompra(
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "carro_id") val carroId: Int,
+    @ColumnInfo(name = "timestamp_cadastro") val cadastro: LocalDate,
+    @ColumnInfo(name = "modelo_id") val modeloId: Int,
+    @ColumnInfo(name = "ano") val ano: Int,
+    @ColumnInfo(name = "combustivel") val combustivel: String,
+    @ColumnInfo(name = "num_portas") val numPortas: Int,
+    @ColumnInfo(name = "cor") val cor: String,
+    @ColumnInfo(name = "nome_modelo") val nomeModelo: String,
+    @ColumnInfo(name = "valor") val valor: Double,
+    @ColumnInfo(name = "data_pedido") val dataPedido: LocalDate,
+    @ColumnInfo(name = "cpf_cliente") val cpfCliente: String,
+    @ColumnInfo(name = "status_pedido") val statusPedido: String
+)
