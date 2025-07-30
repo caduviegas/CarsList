51cb6ddc8e18f4be83c65d2e48877cc8ad605ed5
Eduardo Viegas <engenedu@gmail.com>
2025-07-30 18:29:07 -0300

    🚧 usecase to check if has logged user

---
+++ app/src/main/kotlin/io/github/caduviegas/carslist/domain/usecase/HasLoggedUserUseCase.kt
@@ -0,0 +1,17 @@
+package io.github.caduviegas.carslist.domain.usecase
+
+import io.github.caduviegas.carslist.domain.repository.CarDatabaseRepository
+import io.github.caduviegas.carslist.domain.exception.UserNotLoggedInException
+
+class HasLoggedUserUseCase(
+    private val repository: CarDatabaseRepository
+) {
+    suspend operator fun invoke(): Boolean {
+        return try {
+            repository.getLoggedUser()
+            true
+        } catch (_: UserNotLoggedInException) {
+            false
+        }
+    }
+}
