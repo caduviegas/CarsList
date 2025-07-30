db531df5a05efdcce03035356c370a1431336e2b
Eduardo Viegas <engenedu@gmail.com>
2025-07-30 12:34:05 -0300

    🎨 create application and add icon

---
+++ app/src/main/kotlin/io/github/caduviegas/carslist/CarsListApplication.kt
@@ -0,0 +1,9 @@
+package io.github.caduviegas.carslist
+
+import android.app.Application
+
+class CarsListApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
