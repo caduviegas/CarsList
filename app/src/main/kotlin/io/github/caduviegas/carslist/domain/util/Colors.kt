6f763fe59d0f79a7cf57e00a67375d609655ab84
Eduardo Viegas <engenedu@gmail.com>
2025-07-30 21:32:19 -0300

    💄 new color pallet and theme

---
+++ app/src/main/kotlin/io/github/caduviegas/carslist/domain/util/Colors.kt
@@ -0,0 +1,24 @@
+package io.github.caduviegas.carslist.domain.util
+
+import androidx.compose.ui.graphics.Color
+
+object CarColor {
+    val Primary = Color(0xFFE4B858)
+    val PrimaryVariant = Color(0xFFB8963F)
+    val PrimaryLight = Color(0xFFFFF2D1)
+
+    val Secondary = Color(0xFF9B9893)
+    val SecondaryVariant = Color(0xFF6E6B67)
+    val SecondaryLight = Color(0xFFC7C5C2)
+
+    val Background = Color(0xFFFFFDF7)
+    val Surface = Color(0xFFFFFFFF)
+    val Error = Color(0xFFB00020)
+
+    val OnPrimary = Color(0xFF222222)
+    val OnSecondary = Color(0xFF222222)
+    val OnBackground = Color(0xFF222222)
+    val OnSurface = Color(0xFF222222)
+    val OnError = Color(0xFFFFFFFF)
+}
+
