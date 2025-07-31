ab7c7ffcdc5af057befe4d87638b074bb9b5c57f
Eduardo Viegas <engenedu@gmail.com>
2025-07-30 22:25:37 -0300

    🚧 loginviewmodel fix

--- app/src/main/kotlin/io/github/caduviegas/carslist/di/ViewModelModule.kt
+++ app/src/main/kotlin/io/github/caduviegas/carslist/di/ViewModelModule.kt
@@ -1,10 +1,12 @@
 package io.github.caduviegas.carslist.di
 
 import io.github.caduviegas.carslist.presentation.home.HomeViewModel
+import io.github.caduviegas.carslist.presentation.login.LoginViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 
 val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
 }
---
+++ app/src/main/kotlin/io/github/caduviegas/carslist/presentation/login/LoginUiState.kt
@@ -0,0 +1,8 @@
+package io.github.caduviegas.carslist.presentation.login
+
+sealed class LoginUiState {
+    object Initial : LoginUiState()
+    object Loading : LoginUiState()
+    object Success : LoginUiState()
+    data class Error(val message: String) : LoginUiState()
+}
